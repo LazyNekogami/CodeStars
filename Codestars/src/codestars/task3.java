352e08fbfa41706c0145c4cf92105a8c8a67906a
LazyNekogami <redgarwind@gmail.com>
2015-02-01 22:05:09 +0300

    ..

--- Codestars/src/codestars/task3.java
+++ Codestars/src/codestars/task3.java
@@ -9,40 +9,49 @@ public class task3 {
 
 	public static void main(String[] args) {
 		try (BufferedReader reader = new BufferedReader(new FileReader(
-				"D:\\3.data"))) {
+				"D:\\19.data"))) {
 			String str = reader.readLine();
 			String[] words = str.split(" ");
 			ArrayList<ArrayList<String>> languageGroups = new ArrayList<ArrayList<String>>();
 			languageGroups.add(new ArrayList<String>());
+			task2.print(str);
+			task2.print("1 " + words[0]);
+			task2.print("2 " + words[1]);
+			languageGroups.get(0).add(words[0]);
 			languageGroups.get(0).add(words[1]);
-			languageGroups.get(0).add(words[2]);
-			
-			while(true){
+
+			while (true) {
 				str = reader.readLine();
-				if(str == null){
+				if (str == null) {
 					break;
 				}
 				words = str.split(" ");
 				
-				for(int i = 0; i< languageGroups.size(); i++){
+				boolean enter = false;
+				for (int i = 0; i < languageGroups.size(); i++) {
 					ArrayList<String> temp = languageGroups.get(i);
-					if(temp.contains(words[1]) && !temp.contains(words[2])){
-						temp.add(words[2]);
-					}
-					else if(!temp.contains(words[1]) && temp.contains(words[2])){
+					if (temp.contains(words[0]) && !temp.contains(words[1])) {
 						temp.add(words[1]);
-					} else if(!temp.contains(words[1])&& !temp.contains(words[2])){
-						ArrayList<String> bufferArray =  new ArrayList<String>();
-						bufferArray.add(words[1]);
-						bufferArray.add(words[2]);
-						languageGroups.add(bufferArray);
+						enter = true;
+						break;
+					} else if (!temp.contains(words[0])
+							&& temp.contains(words[1])) {
+						temp.add(words[0]);
+						enter = true;
+						break;
 					}
 				}
-				
-			System.out.println(languageGroups.size());
-				
+				if (!enter) {
+					ArrayList<String> bufferArray = new ArrayList<String>();
+					bufferArray.add(words[0]);
+					bufferArray.add(words[1]);
+					languageGroups.add(bufferArray);
+				}
+
+				System.out.println(languageGroups.size());
+
 			}
-			} catch (IOException ex) {
+		} catch (IOException ex) {
 
 			System.out.println(ex.getMessage());
 		}
